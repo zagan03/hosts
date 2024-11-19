b59ff67432fba743d2bd506caa4d1de947edfcad
zagan03 <zagan_claudiu65@yahoo.com>
2024-11-19 14:00:41 +0000

    Modified script: Added function to validate IP host association with DNS

--- validate_hosts.sh
+++ validate_hosts.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-while read -r line; do
-	[[ $line =~ ^# ]] && continue
+validate_ip() {
+local host=$1
+local ip=$2
+local dns_server=$3
 
-	ip=$(echo "$line" | awk '{print $1}')
-
-	if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
-		echo "Valid IP: $ip"
+	if [[$ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+		resolved_ip=$(dig @$dns_server +short $host)
+		if [[ $resolved_ip == $ip ]]; then
+			echo "Valid association: $host -> $ip"
+		else
+			echo "Invalid association: $host -> $ip"
+		fi
 	else
 		echo "Invalid IP: $ip"
 	fi
-done< /etc/hosts
+}
 
+while read -r line; do
+	[[ $line =~ ^# ]] && continue
+
+	ip =$(echo "$line" | awk '{print $1}')
+	host=$(echo "$line" | awk '{print $2}')
+
+	validate_ip "$host" "$ip" "8.8.8.8"
+done < /etc/hosts
