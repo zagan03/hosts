41014d058b57131ac55f993e0e166add9b7495fa
zagan03 <zagan_claudiu65@yahoo.com>
2024-11-19 14:18:35 +0000

    Add IP validation script for /etc/hosts

--- validate_hosts.sh
+++ validate_hosts.sh
@@ -1,27 +1,37 @@
+
 #!/bin/bash
-
+ 
+# Funcție pentru verificarea unei asocieri IP-host folosind un server DNS
 validate_ip() {
-local host=$1
-local ip=$2
-local dns_server=$3
-
-	if [[$ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
-		resolved_ip=$(dig @$dns_server +short $host)
-		if [[ $resolved_ip == $ip ]]; then
-			echo "Valid association: $host -> $ip"
-		else
-			echo "Invalid association: $host -> $ip"
-		fi
-	else
-		echo "Invalid IP: $ip"
-	fi
+    local host=$1
+    local ip=$2
+    local dns_server=$3
+ 
+    # Verifică dacă adresa este IPv4
+    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+        # Rezolvă numele hostului folosind serverul DNS
+        resolved_ip=$(dig @$dns_server +short $host)
+        if [[ $resolved_ip == $ip ]]; then
+            echo "Valid association: $host -> $ip"
+        else
+            echo "Invalid association: $host -> $ip"
+        fi
+    else
+        echo "Ignoring invalid or IPv6 address: $ip"
+    fi
 }
-
+ 
+# Verifică asocierile din /etc/hosts
 while read -r line; do
-	[[ $line =~ ^# ]] && continue
-
-	ip =$(echo "$line" | awk '{print $1}')
-	host=$(echo "$line" | awk '{print $2}')
-
-	validate_ip "$host" "$ip" "8.8.8.8"
+    [[ $line =~ ^# ]] && continue  # Ignoră liniile comentate
+ 
+    # Extrage adresa IP și numele hostului (doar primele două coloane)
+    ip=$(echo "$line" | awk '{print $1}')
+    host=$(echo "$line" | awk '{print $2}')
+ 
+    # Verifică dacă există ambele coloane
+    if [[ -n $ip && -n $host ]]; then
+        # Verifică asocierea folosind un server DNS (ex: 8.8.8.8 - serverul Google)
+        validate_ip "$host" "$ip" "8.8.8.8"
+    fi
 done < /etc/hosts
